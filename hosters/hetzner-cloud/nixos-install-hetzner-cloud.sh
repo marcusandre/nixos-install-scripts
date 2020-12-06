900043e628a9c3b220d871cf1e3fa7b0eb06c577
Marcus André <marcusandre@users.noreply.github.com>
2020-12-06 16:35:28 +0100

    Update nixos-install-hetzner-cloud.sh

--- hosters/hetzner-cloud/nixos-install-hetzner-cloud.sh
+++ hosters/hetzner-cloud/nixos-install-hetzner-cloud.sh
@@ -64,7 +64,8 @@ echo '
 
   # Replace this by your SSH pubkey
   users.users.root.openssh.authorizedKeys.keys = [
-    "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAtwCIGPYJlD2eeUtxngmT+4yR7BMlK0F5kzj+84uHsxxsy+PXFrP/tScCpwmuoiEYNv/9WKnPJJfCA9XlIDr6cla1MLpaW6eg672TRYMmKzH6SLlkg+kyDmPxSIJw+KdKfnPYyva+Y/VocACYJo0voabUeLAVgtSKGz/AFzccjfOR0GmFO911zjAaR+jFb9M7t7dveNVKm9KbuBfu3giMgGg3/mKz1TKY8yk2ZOxpT5CllBb+B5BcEf+7IGNvNxr1Z0zz5cFXQ3LyBIZklnC/OaQCnD78BSiyPTkIXcmBFal2TaFwTDvki6PuCRpJy+dU1fDdgWLql97D0SVnjmmomw=="
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPtkp+Tv3957mk1nHQ3UsnSao4aszXADJiVxjswLK51k",
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIC57kgjM3I83LWIYACvlp6L03I6HBRtvOVVRoTkkm2wS"
   ];
 }
 ' >> /mnt/etc/nixos/configuration.nix
